44560acf35c521c16817adec8a98219b2bdb738d
aznjp <jpark103193@gmail.com>
2020-10-10 11:59:25 -0400

    restyling the starter html

--- Index.js
+++ Index.js
@@ -28,89 +28,98 @@ const teamQuestions = [{
         name: "Role",
         choices: ["Manager", "Engineer", "Intern"]
     }
-]
+];
 
 const managerQuestion = {
     type: 'input',
     message: 'What is your office number?',
     name: 'OfficeNumber'
-}
+};
 const engineerQuestion = {
     type: 'input',
     message: 'What is your Github Profile?',
     name: 'Github'
-}
+};
 const internQuestion = {
     type: 'input',
     message: "What is your school's name?",
     name: 'School'
-}
+};
 
 const end = {
     type: 'confirm',
     message: "Do you want to add another employee?",
     name: 'End'
-}
+};
 
-function createEmployee() {
-    inquirer.prompt(teamQuestions).then(function(answers) {
-        switch (answers.Role) {
-            case "Manager":
-                // code block
-                createManager(answers)
-                break;
-            case "Engineer":
-                // code block
-                createEngineer(answers)
-                break;
-            case "Intern":
-                // code block
-                createIntern(answers)
-                break;
-        }
 
-    })
-}
+async function createEmployee() {
+    try {
+        inquirer.prompt(teamQuestions).then(function(answers) {
+            switch (answers.Role) {
+                case "Manager":
+                    // code block
+                    createManager(answers)
+                    break;
+                case "Engineer":
+                    // code block
+                    createEngineer(answers)
+                    break;
+                case "Intern":
+                    // code block
+                    createIntern(answers)
+                    break;
+            }
+
+        });
+    } catch (error) {
+        console.log(error)
+    }
+};
 
 function createManager(answers) {
     inquirer.prompt(managerQuestion).then(function(manager) {
-        console.log(manager);
-        console.log(answers);
+        // console.log(manager);
+        // console.log(answers);
         let { Name, Id, Email } = answers;
-        let newManager = new Manager(Name, Id, Email, manager.officeNumber);
+        let newManager = new Manager(Name, Id, Email, manager.OfficeNumber);
         teamArray.push(newManager)
         endQuestions();
     })
-}
+};
 
 function createEngineer(answers) {
     inquirer.prompt(engineerQuestion).then(function(engineer) {
         let { Name, Id, Email } = answers;
-        let newEngineer = new Engineer(Name, Id, Email, engineer.github);
+        let newEngineer = new Engineer(Name, Id, Email, engineer.Github);
         teamArray.push(newEngineer)
         endQuestions();
     })
-}
+};
 
 function createIntern(answers) {
     inquirer.prompt(internQuestion).then(function(intern) {
         let { Name, Id, Email } = answers;
-        let newIntern = new Intern(Name, Id, Email, intern.school);
+        let newIntern = new Intern(Name, Id, Email, intern.School);
         teamArray.push(newIntern)
         endQuestions();
     })
-}
+};
 
 function endQuestions() {
     inquirer.prompt(end).then(function(answers) {
-        if (answers.end == true) {
+        if (answers.End === false) {
+            console.log("ended")
             renderHTML();
         } else {
+            console.log("continue")
             createEmployee();
         }
     })
-}
+};
 
 function renderHTML() {
     console.log(teamArray)
 }
+
+createEmployee();
